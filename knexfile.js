171241ac136ebf8b780206cd5b015f3647881063
Jordan Miller <jordan.j.miller@live.com>
2020-01-03 01:05:10 -0800

    updated file path

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
@@ -31,10 +30,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migrations',     
+      directory: './migrations',     
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './seeds',
     } 
   }
 
