52921bef8c06e5d1cc05b2781686ad325732f2d2
Jordan Miller <jordan.j.miller@live.com>
2020-01-02 22:52:30 -0800

    first endpoint successfully dispatching texts to correct users when called

--- alert/alert-router.js
+++ alert/alert-router.js
@@ -1,31 +1,53 @@
 const router = require('express').Router();
 const userDb = require('../users/users-model.js');
-
 const db = require('../data/db-config');
+
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_NUMBER;
+const myNumber = process.env.MY_PHONE;
+const client = require('twilio')(accountSid, authToken);
 
 
 router.post('/ExtremeCold', async (req, res) => {
     const {city, api_key} = req.body;
+    const cityObj = {city: city};
+    const cold = {extremeCold: true} ;
 
+    // console.log('accountsid', accountSid)
+    // console.log(api_key, process.env.TOP_SECRET_PASSCODE);
+    if (api_key === process.env.TOP_SECRET_PASSCODE){
+        userDb.findByMultiple(cityObj, cold)
+        .then(users => {
+            if (users.length > 0){
+                console.log('USERS RES: ', users);
+                for (let i = 0; i < users.length; i++){
+                    // console.log('number: ' + `+1${users[i].phonenumber}`);
+                    client.messages
+                    .create({
+                        body: 'IT COLD STAY HOME',
+                        from: twilioNumber,
+                        to: `+1${users[i].phonenumber}`,
+                    });
 
-
-    
-    // try{
-    //     const user = await db('users as u')
-    //         .where({'u.id': req.user.id})
-    //         .select('u.id', 'u.username', 'u.name', 'u.email')
-    //         .first();
-    //     if(user){
-    //         res.status(200).json(user)
-    //     }else{
-    //         console.log('get user 404 error', user);
-    //         res.status(404).json({message: `User with id ${req.user.id} not found.`});
-    //     }
-        
-    // }catch(err){
-    //     console.log(err);
-    //     res.status(500).json({message: 'Error getting user information.'});
-    // }
+                    if (i === users.length - 1){
+                    //   console.log(i, users.length-1);
+                        res.status(200).json({message: 'Messages sent successfully'});
+                    }
+                }
+            }
+            else {
+                res.status(404).json({message: `No users found matching ${city} and cold alert`})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+          res.status(500).json(error);
+        });
+    }
+    else{
+        res.status(403).json({ message: 'Invalid API key' });
+    }
 });
 
 module.exports = router;
