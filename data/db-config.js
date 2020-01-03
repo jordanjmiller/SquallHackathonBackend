83b8eb0d45c4cd95aa1c784345bb8f74d01ac854
Jordan Miller <jordan.j.miller@live.com>
2020-01-03 01:22:01 -0800

    minor config fix

--- data/db-config.js
+++ data/db-config.js
@@ -4,6 +4,4 @@ const config = require('../knexfile.js');
 
 const dbEnv = process.env.DB_ENV || 'development';
 
-const db = knex(config.development);
-
-module.exports = db;
+module.exports = knex(config[dbEnv]);
