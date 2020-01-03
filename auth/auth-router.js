eb4a50973f41dfe0c57d855555bf9521bcfd0559
Jordan Miller <jordan.j.miller@live.com>
2020-01-03 01:45:51 -0800

    hopefully fixed deployed login issue

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const db = require('../data/db-config.js');
+const Users = require('../users/users-model.js');
+const {generateToken} = require('./token.js');
 
 router.post('/register', (req, res) => {
     let { email, password } = req.body;
@@ -20,34 +20,25 @@ router.post('/register', (req, res) => {
             res.status(500).json(error);
           });
     }
-  }); 
-
-router.post('/login', (req, res) => {
-    let { email, password } = req.body;
-    // console.log('req body',req.body);
-    // console.log('email', email);
-    // console.log('password',password);
-
-  
-    Users.findBy({ email })
-      .then(user => {
-        // check that passwords match
-        if (user && bcrypt.compareSync(password, user.password)) {
-            req.session.email = email;
-            res.status(200).json({ message: `Welcome ${user.firstname}!` });
-        } else {
-          // we will return 401 if the password or username are invalid
-          // we don't want to let attackers know when they have a good username
-          res.status(401).json({ message: 'Invalid Credentials' });
+  });
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    if(email && password){
+        const user = await db('users as u').where({'u.email': email.toLowerCase()})
+            .select('u.*')
+            .first();
+        if(user && bcrypt.compareSync(password, user.password)){
+            const token = await generateToken(user);
+            res.status(200).json({message: `Welcome ${email.toLowerCase()}`, token, user: {...user, password: undefined}});
+        }else{
+            res.status(403).json({message: 'Invalid email or password'});
         }
-      })
-      .catch(error => {
-          console.log(error);
-        res.status(500).json(error);
-      });
-  }); 
+    }else{
+        res.status(400).json({message: 'Please provide a email and password'});
+    }
+});
 
-  router.get('/logout', (req, res) => {
+router.get('/logout', (req, res) => {
     if(req.session){
         req.session.destroy(err => {
             if(err){
